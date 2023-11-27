795741cf23e31fcb244a96cb22b59a3d931697c9
Luis Wen <69808402+luiswzh@users.noreply.github.com>
2023-11-27 12:59:00 -0500

    Complete 04 exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayToFilter) {
+    let numberOfArguments = arguments.length-1;
+    let filteredArray = arrayToFilter;
 
+    for (let i=1; i<=numberOfArguments; i++){
+        filteredArray = filteredArray.filter((value)=>(value !== arguments[i]));
+    }
+    return filteredArray;
 };
 
 // Do not edit below this line
