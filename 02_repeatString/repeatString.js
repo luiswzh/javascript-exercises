6ed8892cf4e93fc9b4e862302645a9a93c717327
Luis Wen <69808402+luiswzh@users.noreply.github.com>
2023-11-27 12:23:05 -0500

    Complete 02 exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(repeatingText,repetitions) {
+    if(repetitions<0){
+        return 'ERROR';
+    }
+    let repeatedText = '';
+    for (let i=0; i<repetitions; i++){
+        repeatedText += repeatingText;
+    }
+    return repeatedText;
 };
 
 // Do not edit below this line
