c1bc7e13c7683ff45f7d61e7c2ff1b91c7e30f21
Luis Wen <69808402+luiswzh@users.noreply.github.com>
2023-11-27 13:09:40 -0500

    Complete 05 exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(initialInt,finalInt) {
+    if( typeof (initialInt+finalInt) != 'number' ){
+        return 'ERROR';
+    }
+    if (initialInt<0 || finalInt<0){
+        return 'ERROR';
+    }
+    
+    let sum=0;
+    
+    if(finalInt>initialInt){
+        for (let i=initialInt; i<=finalInt; i++){
+            sum+=i;
+        }
+    } else {
+        for (let i=finalInt; i<=initialInt; i++){
+            sum+=i;
+        }
+    }
+    
+    return sum;
 };
 
 // Do not edit below this line
