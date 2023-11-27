1cd4d000c414499250173007ab4f7b1ad384b45a
Luis Wen <69808402+luiswzh@users.noreply.github.com>
2023-11-27 12:29:08 -0500

    Complete 03 exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(_string) {
+    let length = _string.length;
+    let reversedString = '';
+    for (let i=(length-1); i>=0; i--){
+        reversedString += _string[i];
+    }
+    return reversedString
 };
 
 // Do not edit below this line
