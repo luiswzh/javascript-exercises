c7f9233f02ec470910f60908d4e51bbba0422f5e
Luis Wen <69808402+luiswzh@users.noreply.github.com>
2023-11-27 13:36:35 -0500

    Complete 07 exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,25 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  //Formula (x-32)*5/9
+  let converted = (tempF-32)*5/9;
+
+  if((converted%1)!=0){
+    return Number(converted.toFixed(1));
+  }else{
+    return Number(converted.toFixed(0));
+  }
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  //Formula x*5/9+32
+  let converted = tempC*9/5+32;
+
+  if((converted%1)!=0){
+    return Number(converted.toFixed(1));
+  }else{
+    return Number(converted.toFixed(0));
+  }
+  
 };
 
 // Do not edit below this line
